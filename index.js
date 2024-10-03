97dfe1e9216091cebf0a1a4d5ca187a98c9d580d
Pedro Watermann <pedro.programmer.pt@gmail.com>
2024-10-03 12:08:39 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const tela = document.getElementById("tela");
 let btnIgual;
 let btnLimpar;
+
 document.getElementById("igual").addEventListener("click", () => {
-    btnIgual = true
+    btnIgual = true;
 });
-
-document.getElementById("limpar").addEventListener("click", () => {
-    btnLimpar = true
+document.getElementById("limpar").addEventListener('click', () => {
+        btnLimpar = true;
 });
-
 window.onload = () => {
     tela.value = 0;
     btnLimpar = true;
@@ -36,3 +35,148 @@ function calcular() {
         tela.value = "Erro";
     }
 }
+
+function apagaUm() {
+    tela.value = tela.value.slice(0, -1);
+    if (tela.value == "") {
+        tela.value = 0;
+        btnLimpar = true;
+    }
+}
+
+document.addEventListener("keydown", (e) => {
+    switch (e.key) {
+        case "0":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "1":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "2":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "3":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "4":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "5":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "6":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "7":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "8":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "9":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case ".":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "+":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "/":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "*":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "-":
+            if (btnLimpar) {
+                tela.value = e.key;
+                btnLimpar = false;
+            } else {
+                tela.value += e.key;
+            }
+            break;
+        case "c":
+            limpaTela();
+            break;
+        case "=":
+            calcular();
+            break;
+        case "Enter":
+            calcular();
+            break;
+        case "Backspace":
+            apagaUm();
+            break;
+    }
+});
